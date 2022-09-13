905764006d371d357aae14ca2ee722cce05c641f
Davicprd <davicprd@gmail.com>
2022-09-13 17:20:57 -0300

    Nona Questão

---
+++ Fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int f, i = 1, t = 1, t2 = 1,t3;
+    puts("Digite o número do termo da sequência de Fibonacci desejado: \n");
+    scanf("%d", &f);
+    printf("\n---\n1\n");
+    for (i = 1; i <= f-1; i++)
+    {
+        printf("%d\n", t);
+        t3 = t + t2;
+        t2=t;
+        t=t3;
+    
+    }
+}
