e84b49028e7c94b6ef24c93823b9dffff862d7ef
Davicprd <davicprd@gmail.com>
2022-09-13 17:21:23 -0300

    Nona Questão

--- Fibonacci.c
+++ Fibonacci.c
@@ -2,16 +2,15 @@
 
 int main()
 {
-    int f, i = 1, t = 1, t2 = 1,t3;
+    int f, i = 1, t = 1, t2 = 1, t3;
     puts("Digite o número do termo da sequência de Fibonacci desejado: \n");
     scanf("%d", &f);
     printf("\n---\n1\n");
-    for (i = 1; i <= f-1; i++)
+    for (i = 1; i <= f - 1; i++)
     {
         printf("%d\n", t);
         t3 = t + t2;
-        t2=t;
-        t=t3;
-    
+        t2 = t;
+        t = t3;
     }
 }
