8b3d387bb368f39e907040b481e1effd0b9562e7
Francisco Davi Cassundé Prado <113373345+Davicprd@users.noreply.github.com>
2022-09-13 11:35:23 -0300

    Sétima Questão

---
+++ Divisão.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int b,a;
+    float d;
+    puts("Digite um número: \n");
+    scanf("%d",&b);
+    do{
+    puts("Digite outro número: \n");
+    scanf("%d",&a);
+    }while(a==0);
+    d=(float)b/a;
+
+
+printf("O valor da divisão é %.2f",d);
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+}
