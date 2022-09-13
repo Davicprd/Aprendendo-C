10f9ba88e381decb5d33ca092aae09650a7a0ffa
Davicprd <davicprd@gmail.com>
2022-09-13 17:41:18 -0300

    Décima Questão

---
+++ Quadrado.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int d, i=15, q;
+float d2;
+for (i = 15; i <= 90; i++){
+d = ( i / 4);
+d2 = ((float)i / 4);
+if (d == d2){
+q = i * i;
+printf("\nO quadrado do número %d é %d", i, q);
+}
+}
+}
