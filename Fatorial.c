804064028f9bf17ebb908a407f5b0158dcaa571c
Francisco Davi Cassundé Prado <113373345+Davicprd@users.noreply.github.com>
2022-09-13 10:37:52 -0300

    Terceira Questão

---
+++ Fatorial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int d,i,f=1;
+puts("Digite um número para ser fatorado: \n");
+scanf("%d",&d);
+for (i=1;i<=d;i++){
+f=f*i;
+}
+printf("O número fatorado equivale a %d\n",f);
+return 0;
+}
